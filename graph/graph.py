e422b8690ed8de9e28c1196c213a8674a0d99e93
frank-trollmann <frank.trollmann@code.berlin>
2025-05-09 10:46:22 +0200

    feat: added graph representation

---
+++ graph/graph.py
@@ -0,0 +1,21 @@
+
+
+class Graph:
+    def __init__(self): 
+        self.adjacency_list = []
+        self.num_nodes = 0
+        self.num_edges = 0
+
+    def add_edge(self, from_index, to_index):
+        """ adds an edge to the graph """
+        while from_index >= len(self.adjacency_list):
+            self.adjacency_list.append([])
+            self.num_nodes += 1
+
+        self.adjacency_list[from_index].append(to_index)
+        self.num_edges += 1
+    
+    def print_graph(self):
+        """ prints the graph """
+        for index in range(len(self.adjacency_list)):
+            print(f"{index}: {self.adjacency_list[index]}")
