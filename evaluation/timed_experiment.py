389f8457a75794845b5c0749c07f618e91885687
frank-trollmann <frank.trollmann@code.berlin>
2025-05-21 12:44:15 +0200

    feat: timed experiment collects information about failed and sucessful runs.

--- evaluation/timed_experiment.py
+++ evaluation/timed_experiment.py
@@ -18,6 +18,10 @@ class Timed_Experiment:
 
         self.run_times = []
         self.nr_extensions = []
+        self.successful_run_times = []
+        self.unsucessful_run_times = []
+        self.successful_extensions = []
+        self.unsuccesful_extensions = []
         
 
     def run(self):
@@ -35,11 +39,29 @@ class Timed_Experiment:
                 print(f"Running {self.pathfinder_constructor.__name__} on {start_node} to {end_node}")
 
             start = time.time_ns()
-            _, _ = pathfinder.search(start_node, end_node)
+            result, _ = pathfinder.search(start_node, end_node)
             end = time.time_ns()
             time_ns = end - start
             self.run_times.append(time_ns)
             self.nr_extensions.append(pathfinder.get_last_step_count())
+            if result == None:
+                self.unsucessful_run_times.append(time_ns)
+                self.unsuccesful_extensions.append(pathfinder.get_last_step_count())
+            else:
+                self.successful_run_times.append(time_ns)
+                self.successful_extensions.append(pathfinder.get_last_step_count())
+
+    def get_nr_successful_runs(self):
+        """
+            Returns the number of successful runs.
+        """
+        return len(self.successful_run_times)	
+    
+    def get_nr_unsuccessful_runs(self):	
+        """
+            Returns the number of unsuccessful runs.
+        """
+        return len(self.unsucessful_run_times)	
 
     def get_average_time(self):	
         """
@@ -47,11 +69,36 @@ class Timed_Experiment:
         """
         return sum(self.run_times) / len(self.run_times)
 
+    def get_average_successful_time(self):
+        """
+            Returns the average time taken for the successful runs in nanoseconds.
+        """
+        return sum(self.successful_run_times) / len(self.successful_run_times)	
+
+    def get_average_unsuccessful_time(self):
+        """
+            Returns the average time taken for the unsuccessful runs in nanoseconds.
+        """
+        return sum(self.unsucessful_run_times) / len(self.unsucessful_run_times)	
+
+
     def get_max_time(self):
         """
             Returns the maximum time taken for the runs in nanoseconds.
         """
         return max(self.run_times)
+
+    def get_max_successful_time(self):	
+        """
+            Returns the maximum time taken for the successful runs in nanoseconds.
+        """
+        return max(self.successful_run_times)	
+    
+    def get_max_unsuccessful_time(self):	
+        """
+            Returns the maximum time taken for the unsuccessful runs in nanoseconds.
+        """
+        return max(self.unsucessful_run_times)	
     
     def get_min_time(self):
         """
@@ -59,20 +106,68 @@ class Timed_Experiment:
         """
         return min(self.run_times)
     
+    def get_min_successful_time(self):	
+        """
+            Returns the minimum time taken for the successful runs in nanoseconds.
+        """
+        return min(self.successful_run_times)	
+    
+    def get_min_unsuccessful_time(self):	
+        """
+            Returns the minimum time taken for the unsuccessful runs in nanoseconds.
+        """
+        return min(self.unsucessful_run_times)	
+    
     def get_overall_time(self):
         """
             Returns the overall time taken for the runs in nanoseconds.
         """
         return sum(self.run_times)
+
+    def get_overall_successful_time(self):	
+        """
+            Returns the overall time taken for the successful runs in nanoseconds.
+        """
+        return sum(self.successful_run_times)	
     
+    def get_overall_unsuccessful_time(self):	
+        """
+            Returns the overall time taken for the unsuccessful runs in nanoseconds.
+        """
+        return sum(self.unsucessful_run_times)	
+
     def get_nr_extensions(self):
         """
             Returns the overall number of nodes extended
         """
         return sum(self.nr_extensions)
     
+    def get_nr_successful_extensions(self):	
+        """
+            Returns the overall number of nodes extended in successful runs
+        """
+        return sum(self.successful_extensions)	
+    
+    def get_nr_unsuccessful_extensions(self):	
+        """
+            Returns the overall number of nodes extended in unsuccessful runs
+        """
+        return sum(self.unsuccesful_extensions)	
+
     def get_average_extension_time(self):
         """
             Returns the average time taken for one extension
         """
         return self.get_overall_time() / self.get_nr_extensions()
+    
+    def get_average_successful_extension_time(self):	
+        """
+            Returns the average time taken for one extension in successful runs
+        """
+        return self.get_overall_successful_time() / self.get_nr_successful_extensions()	
+    
+    def get_average_unsuccessful_extension_time(self):	
+        """
+            Returns the average time taken for one extension in unsuccessful runs
+        """
+        return self.get_overall_unsuccessful_time() / self.get_nr_unsuccessful_extensions()	
